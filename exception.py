db772cc2b10b6dc251e96abeb8d3bde45848cc2a
bg0csj <stormuser999@hotmail.com>
2019-03-06 15:50:34 +0100

    add handle zero divide

--- exception.py
+++ exception.py
@@ -1,4 +1,13 @@
 """ som exception handling """
 
 """ my own exception raise"""
-raise Exception("hell wrong one")
+#raise Exception("hell wrong one")
+""" handle exception divide by 0"""
+try:
+    a=int(input("Enter the first number :"))
+    b=int(input("Enter second numbe :"))
+    print(a/b)
+except ZeroDivisionError:
+    print("Idiot, you have try to divide with zero")
+else:
+    print("well done")
